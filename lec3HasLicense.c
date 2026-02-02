06f4daf64d46a5bde11b13f31272f73477a3b4bc
tythomassen <tyler.thomassen@icloud.com>
2026-02-02 11:07:50 -0500

    first commit

---
+++ lec3HasLicense.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int age; 
+    int has_license;
+
+    printf("age=%d\nhaslicense=%d\n", age, has_license);
+    
+    return 0; 
+}
---
+++ test.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+    int number = 42;
+    float pi = 3.14159;
+    char letter = 'A';
+    char word[] = "Hello";
+    int hexNumber = 255;
+
+    printf("Integer (%%d): %d\n", number);
+    printf("Float (%%f): %f\n", pi);
+    printf("Float with width/precision (%%8.2f): %8.2f\n", pi);
+    printf("Character (%%c): %c\n", letter);
+    printf("String (%%s): %s\n", word);
+    printf("Hexadecimal (%%x): %x\n", hexNumber);
+
+    return 0;
+}
