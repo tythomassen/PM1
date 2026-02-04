cfd048721ee5ed7ac2414ca979cf820783e5f096
tythomassen <tyler.thomassen@icloud.com>
2026-02-03 20:20:53 -0500

    Complete Part A: Conditionals with nested grading logic

--- lec3HasLicense.c
+++ lec3HasLicense.c
@@ -5,6 +5,8 @@ int main() {
     int has_license;
 
     printf("age=%d\nhaslicense=%d\n", age, has_license);
-    
+    if (age >= 16 && has_license)
+        printf("can drive/n")
+        
     return 0; 
 }
